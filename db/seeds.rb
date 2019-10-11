3b5c909ca954273817cce8531e3c3108dbb50122
Irma Mejia <ijanettmejia@outlook.com>
2019-10-11 08:58:24 -0400

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+items = Item.create([
+    {name: "Ruby Red", size: 0, price: 12.00, image: "/assets/images/rubyred.jpg"},
+    {name: "Coral Foliage", size: 1, price: 14.00, image: "/assets/images/coralfoliage.jpg"},
+    {name: "Dapper Seahorse", size: 1, price: 14.00, image: "/assets/images/dapperseahorses.jpg"},
+    {name: "Silver & Muave", size: 0, price: 12.00, image: "/assets/images/silverandmuave.jpg"},
+    {name: "Honey Bee", size: 0, price: 12.00, image: "/assets/images/buzzybee.jpg"},
+    {name: "Merry & Bright", size: 1, price: 14.00, image: "/assets/images/merryandbright.jpg"},
+    {name: "Royal Blue", size: 0, price: 12.00, image: "/assets/images/royalblue.jpg"},
+    {name: "Oriental Plum", size: 1, price: 14.00, image: "/assets/images/orientalplum.jpg"},
+    {name: "Paw-Pow", size: 1, price: 14.00, image: "/assets/images/pawpow.jpg"},
+    {name: "Black Paisley", size: 0, price: 12.00, image: "/assets/images/blackpaisley.jpg"}
+])
+
