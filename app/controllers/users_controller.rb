89f2a6495ffb35d49c67f44aff979b4138200779
Irma Mejia <ijanettmejia@outlook.com>
2019-10-13 15:40:12 -0400

    Add password to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username)
+        params.require(:user).permit(:username, :password)
     end
 end
