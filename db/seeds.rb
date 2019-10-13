4831f4baa4825f146ccfcbca4e25e1fdce80187a
Irma Mejia <ijanettmejia@outlook.com>
2019-10-13 15:41:05 -0400

    Add data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 items = Item.create([
-    {name: "Ruby Red", size: 0, price: 12.00, image: "/assets/images/rubyred.jpg"},
-    {name: "Coral Foliage", size: 1, price: 14.00, image: "/assets/images/coralfoliage.jpg"},
-    {name: "Dapper Seahorse", size: 1, price: 14.00, image: "/assets/images/dapperseahorses.jpg"},
-    {name: "Silver & Muave", size: 0, price: 12.00, image: "/assets/images/silverandmuave.jpg"},
-    {name: "Honey Bee", size: 0, price: 12.00, image: "/assets/images/buzzybee.jpg"},
-    {name: "Merry & Bright", size: 1, price: 14.00, image: "/assets/images/merryandbright.jpg"},
-    {name: "Royal Blue", size: 0, price: 12.00, image: "/assets/images/royalblue.jpg"},
-    {name: "Oriental Plum", size: 1, price: 14.00, image: "/assets/images/orientalplum.jpg"},
-    {name: "Paw-Pow", size: 1, price: 14.00, image: "/assets/images/pawpow.jpg"},
-    {name: "Black Paisley", size: 0, price: 12.00, image: "/assets/images/blackpaisley.jpg"}
+    {name: "Tropicaloha", size: 1, price: 14.00, image: "https://drive.google.com/uc?export=view&id=19ZQA0ukRoRtIXgqBoe7Fuq6jPS3q5kFj"},
+    {name: "Clifford", size: 0, price: 12.00, image: "https://drive.google.com/uc?export=view&id=1T-pQylFeVuzn9L499VOBespZzMZKoQqo"},
+    {name: "Coral Foliage", size: 1, price: 14.00, image: "https://drive.google.com/uc?export=view&id=1cPE3FIx2Ry3pv41j22Zug03gkz_xZtvF"},
+    {name: "Dapper Seahorse", size: 1, price: 14.00, image: "https://drive.google.com/uc?export=view&id=1NtfdAkS4xry5tjKAgNM_6FR_lqyzkbaX"},
+    {name: "Silver & Muave", size: 0, price: 12.00, image: "https://drive.google.com/uc?export=view&id=1zEGtZg8oixcsJ30paPn_0GZeZ9wsR3S6"},
+    {name: "Honey Bee", size: 0, price: 12.00, image: "https://drive.google.com/uc?export=view&id=1TBuAeELWup9_a6ob7Ax8fJvboVwTmBA7"},
+    {name: "Merry & Bright", size: 1, price: 14.00, image: "https://drive.google.com/uc?export=view&id=1hxty2drNu-F6fnDWAkhYZgldvzM1XkaL"},
+    {name: "Boltie", size: 1, price: 14.00, image: "https://drive.google.com/uc?export=view&id=15sq2-sWdcOrrix1CRvg38MQ-a8frfHGL"},
+    {name: "Peritwinkle", size: 0, price: 12.00, image: "https://drive.google.com/uc?export=view&id=1sIs3wMZg9Czjyrj_EKZvRBjwpUXAx3mI"},
+    {name: "Royal Blue", size: 0, price: 12.00, image: "https://drive.google.com/uc?export=view&id=1O2_plNwbp3TP0GZTCpYSfRWaDThchUEN"},
+    {name: "Oriental Plum", size: 1, price: 14.00, image: "https://drive.google.com/uc?export=view&id=13_qRq70uH8BkZbMeRP9m5z8J_xFb5g94"},
+    {name: "Black Paisley", size: 0, price: 12.00, image: "https://drive.google.com/uc?export=view&id=1DeyRDX6cTPrFHlpmXHg0ftSFw391OzWX"}
 ])
 
