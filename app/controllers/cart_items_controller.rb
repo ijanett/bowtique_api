622ab7100ecde0e0fcfb6c65952bf6ac09a62f06
Irma Mejia <ijanettmejia@outlook.com>
2019-10-18 02:48:40 -0400

    Create CartItems controller actions

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,22 @@
 class CartItemsController < ApplicationController
+    def index
+        cart_items = CartItem.all
+        render json: cart_items
+    end
+
+    def create
+        cart = Cart.find_by(id: params[:cart_id])
+        user = cart.user
+        cart_item = CartItem.new(cart_id: cart.id, item_id: params[:item_id])
+        
+        render json: Cart.new(cart)
+    end
+
+    def destroy
+        cart = Cart.find_by(id: params[:cart_id])
+        cart_item = CartItem.find_by(id: params[:id])
+        cart_item.destroy
+
+        render json: Cart.new(cart)
+    end
 end
