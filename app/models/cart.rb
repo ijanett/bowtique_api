ffac4c8409018c6462765cb5733654e743bd2baf
Irma Mejia <ijanettmejia@outlook.com>
2019-10-18 02:52:12 -0400

    Create cart instance methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,20 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
   has_many :items, through: :cart_items
+
+  def total
+    total = 0
+
+    self.items.each do |item|
+      total += item.price
+    end
+
+    currency_total = sprintf "%.2f", total
+
+    return '$' + currency_total
+  end
+
+  def order_date
+    self.updated_at.strftime('%b %d, %Y')
+  end
 end
