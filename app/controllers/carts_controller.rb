4f39b411d172cafae2d3795112d5cb68bb70a33d
Irma Mejia <ijanettmejia@outlook.com>
2019-10-19 20:39:00 -0400

    Build out carts controller actions

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,22 @@
 class CartsController < ApplicationController
+    def index
+        carts = Cart.all
+        render json: CartSerializer.new(carts)
+    end
+
+    def show
+        cart = Cart.find_by(id: params[:id])
+        render json: CartSerializer.new(cart)
+    end
+
+    def create
+        cart = Cart.new(cart_params)
+        render json: CartSerializer.new(cart)
+    end
+
+    private
+
+    def cart_params
+        params.require(:cart).permit(:user_id, :checkout)
+    end
 end
