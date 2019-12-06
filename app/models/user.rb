2a9cb473fc5e1a2a04f1a982d5ff24c5440d11fb
Irma Mejia <ijanettmejia@outlook.com>
2019-12-05 19:34:18 -0500

    Remove password from schema and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
     has_many :cart_items, through: :carts
     has_many :items, through: :cart_items
     validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
 
     def open_cart
         if self.carts.where('carts.checkout = ?', false).length == 0
--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2019_10_11_034744) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
